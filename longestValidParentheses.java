bd27f181d0961d67b12df680c7c3ea3a5b7a3750
Dikshant Chauhna <dikshantchauhan93@gmail.com>
2026-07-23 09:34:37 +0530

    Initial commit

---
+++ longestValidParentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int len = 0;
+        stack.push(-1);
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                stack.push(i);
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty())
+                {
+                    stack.push(i);
+                }
+                len = Math.max(len,i-stack.peek());
+            }
+        }
+        return len;
+    }
+}
